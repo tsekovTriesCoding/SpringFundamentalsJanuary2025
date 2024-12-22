a3a5f3beafaa2326a0a517e7483d83470e9e5091
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-12-22 16:13:02 +0200

    add do login

--- 04.ExamPreparation/SpringFundamentalsRegularExam25June2022/src/main/java/com/example/spotifyplaylistapp/controller/UserControllerImpl.java
+++ 04.ExamPreparation/SpringFundamentalsRegularExam25June2022/src/main/java/com/example/spotifyplaylistapp/controller/UserControllerImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -19,6 +20,11 @@ public class UserControllerImpl implements UserController {
     public UserControllerImpl(LoggedUser loggedUser, UserService userService) {
         this.loggedUser = loggedUser;
         this.userService = userService;
+    }
+
+    @ModelAttribute(name = "loginDTO")
+    public LoginDTO loginDTO() {
+        return new LoginDTO();
     }
 
     @Override
