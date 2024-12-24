a13ad20aec33cee2d5756a33c6196b1d08ea2319
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-12-24 18:05:07 +0200

    init shopping list app

---
+++ 04.ExamPreparation/SpringFundamentalsRetakeExam15December2021/src/main/java/bg/softuni/springfundamentalsretakeexam15december2021/SpringFundamentalsRetakeExam15December2021Application.java
@@ -0,0 +1,13 @@
+package bg.softuni.springfundamentalsretakeexam15december2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFundamentalsRetakeExam15December2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFundamentalsRetakeExam15December2021Application.class, args);
+	}
+
+}
---
+++ 04.ExamPreparation/SpringFundamentalsRetakeExam15December2021/src/test/java/bg/softuni/springfundamentalsretakeexam15december2021/SpringFundamentalsRetakeExam15December2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.springfundamentalsretakeexam15december2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFundamentalsRetakeExam15December2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
