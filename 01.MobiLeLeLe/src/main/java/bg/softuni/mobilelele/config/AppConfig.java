e8e8bcddf456767b8880fab4b2d8b23c82460778
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-12-26 21:08:45 +0200

    add app config

---
+++ 01.MobiLeLeLe/src/main/java/bg/softuni/mobilelele/config/AppConfig.java
@@ -0,0 +1,33 @@
+package bg.softuni.mobilelele.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+@Configuration
+public class AppConfig {
+    public AppConfig() {
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
