be15f43d4059d5f2252bb46273621d07a630fb2d
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-12-26 21:08:58 +0200

    change name

--- 01.Mobilelele/src/main/java/bg/softuni/mobilelele/MobileleleApplication.java
+++ 01.Mobilelele/src/main/java/bg/softuni/mobilelele/MobileleleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MobileleleApplication {
+public class MobiLeLeLeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MobileleleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MobiLeLeLeApplication.class, args);
+    }
 
 }
