c3a4e0db5264461db9b46650e9275a43fcaed6ba
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-12-27 21:51:46 +0200

    add do login

--- 04.ExamPreparation/SpringFundamentalsRetakeExam7April2021/src/main/java/bg/softuni/coffeeshop/controller/UserController.java
+++ 04.ExamPreparation/SpringFundamentalsRetakeExam7April2021/src/main/java/bg/softuni/coffeeshop/controller/UserController.java
@@ -5,7 +5,6 @@ import bg.softuni.coffeeshop.service.UserService;
 import bg.softuni.coffeeshop.util.LoggedUser;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -31,7 +30,7 @@ public class UserController {
     }
 
     @GetMapping("/login")
-    String login(Model model) {
+    String login() {
         if (this.loggedUser.isLogged()) {
             return "redirect:/home";
         }
