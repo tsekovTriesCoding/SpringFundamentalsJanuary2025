dadd47d0be9ddacf471b99647dc29c9ed5ab08d0
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-12-27 21:23:18 +0200

    add index and home pages views

---
+++ 04.ExamPreparation/SpringFundamentalsRetakeExam7April2021/src/main/java/bg/softuni/coffeeshop/controller/HomeController.java
@@ -0,0 +1,35 @@
+package bg.softuni.coffeeshop.controller;
+
+import bg.softuni.coffeeshop.util.LoggedUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    private final LoggedUser loggedUser;
+
+    public HomeController(LoggedUser loggedUser) {
+        this.loggedUser = loggedUser;
+    }
+
+    @GetMapping
+    String index() {
+        if (loggedUser.isLogged()) {
+            return "redirect:/home";
+        }
+
+        return "index";
+    }
+
+    @GetMapping("home")
+    String home(Model model) {
+        if (!loggedUser.isLogged()) {
+            return "redirect:/";
+        }
+
+        return "home";
+    }
+}
