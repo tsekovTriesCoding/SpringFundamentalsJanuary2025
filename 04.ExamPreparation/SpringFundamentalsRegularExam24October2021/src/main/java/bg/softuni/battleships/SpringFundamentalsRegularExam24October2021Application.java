b6d67c8d327f68bcda60087b64f06b23451dda9b
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-12-27 10:10:37 +0200

    init battleships app

---
+++ 04.ExamPreparation/SpringFundamentalsRegularExam24October2021/src/main/java/bg/softuni/battleships/SpringFundamentalsRegularExam24October2021Application.java
@@ -0,0 +1,13 @@
+package bg.softuni.battleships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFundamentalsRegularExam24October2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFundamentalsRegularExam24October2021Application.class, args);
+    }
+
+}
