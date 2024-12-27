3992aef8c5b731f5a2ebc584fac6c7b935bdae72
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-12-27 20:55:31 +0200

    init coffeeshop app

---
+++ 04.ExamPreparation/SpringFundamentalsRetakeExam7April2021/src/main/java/bg/softuni/coffeeshop/SpringFundamentalsRetakeExam7April2021Application.java
@@ -0,0 +1,13 @@
+package bg.softuni.coffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFundamentalsRetakeExam7April2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFundamentalsRetakeExam7April2021Application.class, args);
+    }
+
+}
