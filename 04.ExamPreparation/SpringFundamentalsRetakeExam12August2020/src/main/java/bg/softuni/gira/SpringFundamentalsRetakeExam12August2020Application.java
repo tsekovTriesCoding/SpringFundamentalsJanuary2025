3acf41501c65dfee2e5372502736974f2eca315e
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-12-31 14:36:39 +0200

    init gira app

---
+++ 04.ExamPreparation/SpringFundamentalsRetakeExam12August2020/src/main/java/bg/softuni/gira/SpringFundamentalsRetakeExam12August2020Application.java
@@ -0,0 +1,13 @@
+package bg.softuni.gira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFundamentalsRetakeExam12August2020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFundamentalsRetakeExam12August2020Application.class, args);
+    }
+
+}
---
+++ 04.ExamPreparation/SpringFundamentalsRetakeExam12August2020/src/test/java/bg/softuni/gira/SpringFundamentalsRetakeExam12August2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.gira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFundamentalsRetakeExam12August2020ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
