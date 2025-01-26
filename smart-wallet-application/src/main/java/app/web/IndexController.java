3917411e8e00373175deeb2aca0ce39838278900
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-01-26 15:28:08 +0200

    register

--- smart-wallet-application/src/main/java/app/web/IndexController.java
+++ smart-wallet-application/src/main/java/app/web/IndexController.java
@@ -2,9 +2,13 @@ package app.web;
 
 import app.user.model.User;
 import app.user.service.UserService;
+import app.web.dto.RegisterRequest;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.UUID;
@@ -32,9 +36,26 @@ public class IndexController {
     }
 
     @GetMapping("/register")
-    public String getRegisterPage() {
+    public ModelAndView getRegisterPage() {
 
-        return "register";
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("register");
+        modelAndView.addObject("registerRequest", new RegisterRequest());
+
+        return modelAndView;
+    }
+
+    @PostMapping("/register")
+    public ModelAndView registerNewUser(@Valid RegisterRequest registerRequest, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return new ModelAndView("register");
+        }
+
+
+        userService.register(registerRequest);
+
+        return new ModelAndView("redirect:/home");
     }
 
     @GetMapping("/home")
--- smart-wallet-application/src/main/java/app/web/dto/RegisterRequest.java
+++ smart-wallet-application/src/main/java/app/web/dto/RegisterRequest.java
@@ -3,11 +3,15 @@ package app.web.dto;
 import app.user.model.Country;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Builder
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
 
     @Size(min = 6, message = "Username must be at least 6 symbols")
